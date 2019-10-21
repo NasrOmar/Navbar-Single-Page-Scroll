105057ee89516b5e2fbac3817cc55547539f212f
Omar Nasr <nasr.omar@icloud.com>
2019-10-21 12:49:07 +0200

    navigation sticky function via JavaScript

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,24 @@
 // JavaScript Document
 
+// Navbar Toggler
 function navMenu() {
 	var navToggler = document.getElementById("navbar-toggler");
 	var navHeight = document.getElementById("nav-items");
 	
 	navToggler.classList.toggle("navbar-toggler-btn-x");
 	navHeight.classList.toggle("nav-items-height");
+}
+
+// Navbar sticky function
+window.onscroll = function() {navFixed()};
+
+var navHeader = document.getElementById("navbar");
+var navSticky = navHeader.offsetTop;
+
+function navFixed() {
+	if (window.pageYOffset > navSticky) {
+		navHeader.classList.add("navbar-sticky");
+	} else {
+		navHeader.classList.remove("navbar-sticky");
+	}
 }
