99a0063898866f87e02964743143ff9ee4c4752b
Omar Nasr <nasr.omar@icloud.com>
2019-10-21 10:19:30 +0200

    navbar toggler added

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function navMenu() {
+	var navToggler = document.getElementById("navbar-toggler");
+	var navHeight = document.getElementById("nav-items");
+	
+	navToggler.classList.toggle("navbar-toggler-btn-x");
+	navHeight.classList.toggle("nav-items-height");
+}
