ce0fa34f1240a9e733b837069a13cbb830371240
Omar Nasr <nasr.omar@icloud.com>
2019-10-21 14:58:31 +0200

    adjustments

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,30 @@
 // JavaScript Document
 
+// Back to Top Scroll
 function backToTop() {
-	scrollTo(0,0);
+	window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+// To Section Scroll Smooth
+const navLinks = document.querySelectorAll("nav ul li a");
+
+var i;
+
+for (i = 0; i < navLinks.length; i++) {
+	const href = navLinks[i].getAttribute("href");
+	const section = document.querySelector(href);
+	const offset = 60;
+	
+	navLinks[i].onclick = function(navClick) {
+		const bodyRect = document.body.getBoundingClientRect().top;
+		const sectionRect = section.getBoundingClientRect().top;
+		const sectionPosition = sectionRect - bodyRect;
+		const offsetPosition = sectionPosition - offset;
+		
+		navClick.preventDefault();
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: "smooth"
+		})
+	}
 }
